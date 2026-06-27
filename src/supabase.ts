e8d4c0994b9e0cc0fabf7068eb65521d20b4710c
Viniciusmartins-Quanta <vinicius.martins@quantaconsultoria.com>
2026-06-27 13:52:41 -0300

    update credencials

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = (import.meta as any).env.VITE_SUPABASE_URL || "https://jaopixixqfcbszwuceuc.supabase.co";
-const supabaseAnonKey = (import.meta as any).env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imphb3BpeGl4cWZjYnN6d3VjZXVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4Nzk1OTksImV4cCI6MjA5NzQ1NTU5OX0.Kg3VkiB7ulWsiyH93vnz99rbDnKHt6jl4VYehKK2Co8";
-const supabasePublishableKey = (import.meta as any).env.VITE_SUPABASE_PUBLISHABLE_KEY || "sb_publishable_fkjiMmc4n_8-bcalSaYoIA_hgGPIZN3"
+const supabaseUrl = (import.meta as any).env.VITE_SUPABASE_URL || "https://sfxyybvhxntsyfyiinov.supabase.co";
+const supabaseAnonKey = (import.meta as any).env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmeHl5YnZoeG50c3lmeWlpbm92Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4ODcwNDQsImV4cCI6MjA5NzQ2MzA0NH0.kbBekuJeGhwaLeJHCP8rQhUsNE0ba4XIMfGVkLw26rA";
+const supabasePublishableKey = (import.meta as any).env.VITE_SUPABASE_PUBLISHABLE_KEY || "sb_publishable_9kEf6iB3I52g-MKMn8KqSA_8OzK82zf"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
